252128682563583142f0e7a4b3489ce036bbdcac
phoenix-er <75857038+phoenix-er@users.noreply.github.com>
2022-05-05 18:20:18 +0000

    Create sol9.cpp

---
+++ Assignment1/sol9.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(){
+    int n,a[100000],maxm=0,j=0; 
+    cin>>n; 
+    for(int i=0;i<n;i++){ 
+    cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        maxm=max(maxm,a[i]);
+    } 
+    for(int i=0;i<n;i++){
+       if(a[i]==maxm){ 
+          j++; 
+        } 
+     }
+    cout<<j;
+}
