33f45dc0f9ca64b0f0cf3d64f5b27c5c54494835
phoenix-er <75857038+phoenix-er@users.noreply.github.com>
2022-05-18 17:15:29 +0000

    Create selection_sort.cpp

---
+++ Assignment 7.1/selection_sort.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int key;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i;j<nums.size()-1;j++)
+            if(nums[j+1]<nums[i]){
+                 key=nums[i];
+                 nums[i]=nums[j+1];
+                 nums[j+1]=key;                
+            }
+            
+        }
+        return nums;
+    }
+};
