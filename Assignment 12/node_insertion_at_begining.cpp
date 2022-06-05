7f71ef9659e388162d9c9bf1db929700a8180aea
Mohd Eraf <75857038+phoenix-er@users.noreply.github.com>
2022-06-05 18:14:52 +0000

    Update node_insertion_at_begining.cpp

--- Assignment 12/node_insertion_at_begining.cpp
+++ Assignment 12/node_insertion_at_begining.cpp
@@ -1,6 +1,9 @@
 SinglyLinkedListNode* insertNodeAtHead(SinglyLinkedListNode* llist, int data) {
     
     SinglyLinkedListNode* ptr = new SinglyLinkedListNode(data);
+    ptr->data= data;
     ptr ->next = llist;
-    return ptr; 
+    return ptr;
+    
+    
 }
