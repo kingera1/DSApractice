1fb36a8a4bbc206a14701ce3e272b827734bf048
Mohd Eraf <75857038+m-eraf@users.noreply.github.com>
2023-01-25 18:41:59 +0000

    Create maximum_subarray.cpp

---
+++ ptc/maximum_subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0;
+        int mx=INT_MIN;
+        for(int i =0;i<nums.size();i++){
+            sum = sum+nums[i];
+            mx = max(sum,mx);
+            if(sum<0){
+                sum=0;
+            }
+        }
+    return mx;
+    }
+};
